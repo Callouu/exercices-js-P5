dc79fb169e5fa0c05b14385fc275240e700d236a
Alexis Bouvry <bouvryalexis@gmail.com>
2025-01-07 22:17:12 +0100

    Update Excercices

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,11 @@
 //votre code ici
 
+function printNumbers(start) {
+	var myArray = [];
+	for (let i = 1; i <= start; i++) {
+		myArray.push(i);
+		}
+	return myArray.join(" ");
+}
 export default printNumbers
+
--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,3 +1,16 @@
 // votre code ici
 
+function checkAge(a) {
+    let age = parseInt(a);
+    if (age < 18) {
+        return "Vous êtes mineur."
+    }
+    else if (age >= 18 && age < 65) {
+        return "Vous êtes majeur."
+    }
+    else {
+        return "Vous êtes senior."
+    }
+}
+
 export default checkAge;
